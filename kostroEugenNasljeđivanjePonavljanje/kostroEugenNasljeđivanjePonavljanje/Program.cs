d4567be4942b4ebad23d5e20088c4b0a324fa5dc
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2020-10-13 10:47:48 +0200

    Add files via upload

---
+++ kostroEugenNasljeđivanjePonavljanje/kostroEugenNasljeđivanjePonavljanje/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenNasljeđivanjePonavljanje
+{
+    interface IControl
+    {
+        void Metoda();
+
+       int Brojac { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComboBox x = new ComboBox();
+            ListBox y = new ListBox();
+            x.Metoda();
+            y.Metoda();
+            Console.ReadKey();
+        }
+    }
+
+    class ListBox : IControl
+    {
+        public int Brojac
+        {
+            get { return Brojac; }
+            set { Brojac = value; }
+        }
+
+        
+
+        public void Metoda()
+        {
+            Console.WriteLine("Metoda List");
+            
+        }
+    }
+
+    class ComboBox : IControl
+    {
+        public int Brojac
+        {
+            get { return Brojac; }
+            set { Brojac = value; }
+        }
+
+
+
+        public void Metoda()
+        {
+            Console.WriteLine("Metoda Combo");
+        }
+    }
+
+}
